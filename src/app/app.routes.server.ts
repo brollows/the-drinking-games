87de965d0c84b10c63318cffb924792da493ac03
Erik Faane <faane@hsnews.no>
2025-12-10 16:00:11 +0100

    Fixed an issue where netlify tried to pre-render a dynamic page

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Runde-siden må IKKE prerendres
+  {
+    path: 'round/:sessionId',
+    renderMode: RenderMode.Server, // eller RenderMode.Client
+  },
+
+  // Alle andre ruter kan prerendres
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   }
 ];
